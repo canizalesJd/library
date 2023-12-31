45c322ac0136c7f363cb97998517e65adedc777c
Jose Canizales <canizalesjd@gmail.com>
2023-12-31 17:53:42 -0600

    Basic form functionallity

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const form = document.querySelector(".new-book-form");
 
 class Book {
 	constructor(title, author, pages, read = false) {
@@ -6,12 +7,38 @@ class Book {
 		this.author = author;
 		this.pages = pages;
 		this.read = read;
-		this.info = () => {
-			return `${this.title}, by ${this.author}, ${this.pages} pages, ${
-				this.read ? "read" : "not read yet"
-			}`;
-		};
 	}
 }
 
-function addBookToLibrary() {}
+const addBookToLibrary = (title, author, pages, read) => {
+	myLibrary.push(new Book(title, author, pages, read));
+};
+
+const formControl = (display = false) => {
+	display ? (form.style.display = "block") : (form.style.display = "none");
+};
+
+const resetForm = () => {
+	document.getElementById("title").value = "";
+	document.getElementById("author").value = "";
+	document.getElementById("pages").value = "";
+	document.getElementById("read").checked = false;
+	formControl(false);
+	// displayLibrary();
+};
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const title = document.getElementById("title").value;
+	const author = document.getElementById("author").value;
+	const pages = document.getElementById("pages").value;
+	const read = document.getElementById("read").checked;
+	addBookToLibrary(title, author, pages, read);
+	resetForm();
+	console.log(myLibrary);
+});
+
+const addButton = document.getElementById("add-book");
+addButton.addEventListener("click", () => {
+	formControl(true);
+});
